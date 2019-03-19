86111d2837f78667c45cd08e4f1375e6b148cf79
braudelan <braudelan@gmail.com>
2019-03-19 17:04:48 +0200

    qCO2.py data upload added visualization added

--- correlations.py
+++ correlations.py
@@ -24,13 +24,22 @@ for test, number in zip(TESTS, NUMBERS):
     baseline_means = means.xs('t', level=1, axis=1).loc[0]
 
     stats[test + '_means'] = means
-    stats[test + ' baseline'] = baseline_means
+    stats[test + '_baseline'] = baseline_means
 
-baseline_params = ['MBC', 'ERG', 'TOC', 'AS']
+independent_keys = ['MBC', 'ERG', 'TOC', 'AS']
+dependent_keys = ['MBC', 'MBN', 'DOC', 'HWE-S', 'ERG','RESP', 'AS','TOC']
 
-for p in baseline_params :
+independent_params = [stats[key + '_baseline'] for key in independent_keys]
+dependent_params = [stats[key + '_means'] for key in dependent_keys]
 
-    figure =
-    figure.add_subplot()
+i = 1
+
+for ind_p in independent_params :
+
+    figure = pyplot.figure(i)
+
+    for dep_p in dependent_params:
+        for day in dep
+        figure.add_subplot()
 
 
