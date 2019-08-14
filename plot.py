0bf06986710f806671367f6e29cd93b6e21b1e3a
elan <braudelan@gmail.com>
2019-08-14 15:03:05 +0300

    plot.py quite a few adjustments

--- plot.py
+++ plot.py
@@ -9,21 +9,16 @@ from matplotlib.lines import Line2D
 from matplotlib.ticker import MultipleLocator, NullLocator
 from matplotlib.figure import Figure
 from matplotlib.axes import Axes
-# from scipy.optimize import curve_fit
-# from sklearn.metrics import r2_score
-# from pandas import Series
 
 from stats import get_stats, get_baseline
 from helpers import Constants
 
 
-# constants
 SOILS = Constants.soils
 MARKERS = Constants.markers
 COLORS = Constants.colors
-# pyplot parameters
 
-# pyplot.style.use('ggplot')
+# pyplot configuration
 
 pyplot.rc(
           'legend',
@@ -32,9 +27,10 @@ pyplot.rc(
           markerscale=2
   )
 
-pyplot.rc('font', size=19) # control text size when not defined locally
+pyplot.rc('font', size=15) # control text size when not defined locally
 pyplot.rc('lines', linewidth=5)
 # pyplot.rc('marker', size=5)
+# pyplot.style.use('ggplot')'
 
 symbol_text_params = {'weight': 'bold',
                       'size': 26,
@@ -42,14 +38,44 @@ symbol_text_params = {'weight': 'bold',
 
 line_styles = densly_dashed, solid = ((0, (2, 1)), (0, ()))
 
-major_locator = MultipleLocator(7)  # major ticks locations
-minor_locator = MultipleLocator(1)  # minor ticks locations
+MAJOR_LOCATOR = MultipleLocator(7)  # major ticks locations
+MINOR_LOCATOR = MultipleLocator(1)  # minor ticks locations
 
 
-# todo adjust plot_dynamics for plotting a single axes (e.g. control)
-# todo for MRE_&_normalized work on legend and x_label
-# todo work on MRE_notation_marks()
-# todo change rcparams for marker size to stand
+def MRE_notation_marks(axes: Axes):
+
+    MRE_TIME_POINTS = [0, 7, 14 ] # days when MRE was applied
+
+    arrow_angle = 0.7 # radians from a downwards line perpendicular to x axis
+    offset_head_x = 0.3 # offset of arrow head from annotation point, given in data coordinates(=days)
+    offset_base_x = offset_head_x + math.sin(arrow_angle) # offset of arrow base
+    head_y = -0.03 # fraction of axes size
+    base_y = head_y -0.07 # fraction of axes size
+
+    arrow_properties = dict(
+                            arrowstyle="wedge,tail_width=0.7",
+                            fc="0.6",
+                            ec="0.1",
+                           )
+
+    for time_point in MRE_TIME_POINTS:
+
+      # axes.annotate(
+      #               s='',
+      #               xy=(time_point - offset_head_x, head_y ), # arrow head coordinates
+      #               xytext=(time_point - offset_base_x, base_y), # arrow base coordinates
+      #               xycoords=('data', 'axes fraction'),
+      #               arrowprops=arrow_properties,
+      #               )
+      axes.annotate(
+                    s='',
+                    xy=(time_point - offset_head_x, head_y ), # arrow head coordinates
+                    xytext=(time_point - offset_base_x, base_y), # arrow base coordinates
+                    xycoords=('data','axes fraction'),
+                    textcoords=('data', 'axes fraction'),
+                    arrowprops=(arrow_properties)
+                   )
+
 
 def make_figure(data, number, data_set_name):
 
@@ -68,7 +94,13 @@ def make_figure(data, number, data_set_name):
     return figure
 
 
-def make_line_axes(figure: Figure, last_day, major_locator, minor_locator, x_label='', y_label='', axes_lineup=0) -> Axes:
+def make_line_axes(figure: Figure, data, axes_lineup=0) -> Axes:
+    '''add an axes with basic configuration to a given figure.'''
+
+    last_day = data.index[-1]
+    max_value = data.max().max() # highest value measured in any of the soils
+    percent_extra_space = 0.2
+    upper_ylim = max_value + max_value * percent_extra_space
 
     # allocate position of axes in figure (0=single axes, 1=first out of two, else=second out of two)
     position = (
@@ -77,24 +109,18 @@ def make_line_axes(figure: Figure, last_day, major_locator, minor_locator, x_lab
                     212
                     )
 
-    axes = figure.add_subplot(position)
+    axes: Axes = figure.add_subplot(position)
 
-    axes.set_xlim((0, last_day))
-    # axes.set_ylim(0, max_value)
-    axes.xaxis.set_minor_locator(minor_locator)
-    axes.xaxis.set_major_locator(major_locator)
+    axes.set_xlim(0, last_day)
+    axes.set_ylim(0, upper_ylim)
+    axes.xaxis.set_minor_locator(MINOR_LOCATOR)
+    axes.xaxis.set_major_locator(MAJOR_LOCATOR)
     axes.tick_params(axis='x', which='minor', width=1, length=3)
-    axes.set_ylabel(y_label, labelpad=30) if bool(y_label) == True else None
-
-    if axes_lineup == 2 or axes_lineup == 0:
-        MRE_notation_marks(axes)  # add arrows where MRE was applied
-    if axes_lineup == 1:
-        axes.get_xaxis().set_visible(False)
 
     return axes
 
 
-def make_lines(axes, data, stde=None):
+def make_lines(axes: Axes, data, stde=None):
 
 
     """
@@ -156,7 +182,7 @@ def make_lines(axes, data, stde=None):
     return lines
 
 
-def plot_dynamics(figure, data, stde, set_name, axes_lineup, stdv=None):
+def plot_dynamics(figure: Figure, data, stde, set_name, axes_lineup):
 
     last_day = data.index[-1]
     is_normalized = True if axes_lineup == 2 else False # check if the data being plotted is normalized
@@ -165,22 +191,28 @@ def plot_dynamics(figure, data, stde, set_name, axes_lineup, stdv=None):
     xlabel_text = r'$incubation\ time\ \slash\ days$'
     normalized = 'normalized' if is_normalized else ''
     if set_name == 'RESP':
-        ylabel_text = r'$%s\ %s\ mg\ CO_{2}-C\ ' \
-                      r'\ast\ kg\ soil^{-1}\ \ast\ day^{-1} $' %(normalized, set_name)
+        ylabel_text = r'$%s\ %s\ \slash$' '\n' r'$mg\ CO_{2}-C\ $'  \
+                      r'$\ast\ kg\ soil^{-1}\ \ast\ day^{-1} $' %(normalized, set_name)
     else:
-        ylabel_text = r'$%s\ %s\ \slash\ mg' \
-                      r' \ast kg\ soil^{-1}$' %(normalized, set_name)
+        ylabel_text = r'$%s\ %s\ \slash$' '\n' \
+                      r'$mg\ \ast kg\ soil^{-1}$' %(normalized, set_name)
 
-    # create means axes and set parameters
-    axes = make_line_axes(figure, last_day, major_locator, minor_locator,
-                                    x_label='', y_label=ylabel_text, axes_lineup=axes_lineup)
+    ylabel_rotation = 65 if set_name == 'RESP' else 45
 
-    # shared x label for figure with two plots
-    if axes_lineup == 2 or axes_lineup == 0:
-        axes.set_xlabel(xlabel_text, labelpad=40)
+    # create axes
+    axes = make_line_axes(figure, data, axes_lineup=axes_lineup)
 
     # plot data on axes
     lines = make_lines(axes, data, stde) # todo take out specific data points    (MBC)
+
+    # customize axes
+    if axes_lineup == 2 or axes_lineup == 0:
+        axes.set_xlabel(xlabel_text, labelpad=40) # x label
+        MRE_notation_marks(axes)  # add arrows where MRE was applied
+    if axes_lineup == 1:
+        axes.get_xaxis().set_visible(False)
+
+    axes.set_ylabel(ylabel_text, labelpad=50, rotation=ylabel_rotation)
 
     # customize legend
     list_lines = list(lines.items())  #e.g.'ORG': <ErrorbarContainer object of 3 artists>
@@ -194,43 +226,7 @@ def plot_dynamics(figure, data, stde, set_name, axes_lineup, stdv=None):
     figure.legend(handles, lables, loc='center right')  # todo remove error bars from legend objects.
 
 
-def MRE_notation_marks(axes):
-
-    MRE_TIME_POINTS = [0, 7, 14 ] # days when MRE was applied
-
-    arrow_angle = 0.7 # radians from a downwards line perpendicular to x axis
-    offset_head_x = 0.3 # offset of arrow head from annotation point, given in data coordinates(=days)
-    offset_base_x = offset_head_x + math.sin(arrow_angle) # offset of arrow base
-    head_y = -0.03 # fraction of axes size
-    base_y = head_y -0.07 # fraction of axes size
-
-    arrow_properties = dict(
-                            arrowstyle="wedge,tail_width=0.7",
-                            fc="0.6",
-                            ec="0.1",
-                            # connectionstyle="arc3,rad=0.5"
-                           )
-
-    for time_point in MRE_TIME_POINTS:
-
-      # axes.annotate(
-      #               s='',
-      #               xy=(time_point - offset_head_x, head_y ), # arrow head coordinates
-      #               xytext=(time_point - offset_base_x, base_y), # arrow base coordinates
-      #               xycoords=('data', 'axes fraction'),
-      #               arrowprops=arrow_properties,
-      #               )
-      axes.annotate(
-                    s='',
-                    xy=(time_point - offset_head_x, head_y ), # arrow head coordinates
-                    xytext=(time_point - offset_base_x, base_y), # arrow base coordinates
-                    xycoords=('data','axes fraction'),
-                    textcoords=('data', 'axes fraction'),
-                    arrowprops=(arrow_properties)
-                   )
-
-
-def plot_all_parameters(raw_data_sets: dict) -> Figure:
+def plot_baseline(raw_data_sets: dict) -> Figure: #todo x_ticks labels, x_label, unclip y_label
     """plot baseline values of multiple data sets.
 
     this function takes only the control replicates from every data set(=category or analysis)
@@ -252,94 +248,78 @@ def plot_all_parameters(raw_data_sets: dict) -> Figure:
     SOIL_WIDTH = CATEGORY_WIDTH / 2
 
     # labels
-    baseline_y_label = r'normalized means \\' + '\n' + 'percent of smallest mean'
-    growth_y_label = ''
-    x_label = ''
+    baseline_y_label = r'$mean\ baseline\ value\ \slash$'\
+                       + '\n' + r'$\%\ UNC$'
+    x_label = 'soil properties'
 
     # figure
-    basline_growth_figure = pyplot.figure(figsize=(15,10))
+    basline_figure = pyplot.figure(figsize=(15,10))
 
-    basline_growth_figure.subplots_adjust(hspace=0)
+    basline_figure.subplots_adjust(hspace=0)
 
     # baseline_axes
-    baseline_axes = make_bar_axes(basline_growth_figure, X_LOCATIONS, SOIL_WIDTH, baseline_y_label, axes_lineup=1)
+    baseline_axes = make_bar_axes(basline_figure,
+                                  X_LOCATIONS, SOIL_WIDTH,
+                                  baseline_y_label)
 
-    # growth axes
-    growth_axes = make_bar_axes(basline_growth_figure, X_LOCATIONS, SOIL_WIDTH, growth_y_label,
-                                                        x_label=x_label, categories= CATEGORIES, axes_lineup=2)
-    all_bars = {}
-    for x_location, data_set, category_name in zip(X_LOCATIONS, CATEGORIES_DATA, CATEGORIES):
-
-
+    bars = {}
+    for x_location, data_set, category_name in zip(X_LOCATIONS, #todo turn into a function
+                                                   CATEGORIES_DATA,
+                                                   CATEGORIES):
         # baseline data
         baseline_statistics = get_baseline(data_set)
-        baseline = baseline_statistics[0]
-        baseline_SD = baseline_statistics[1]
-        baseline_SE = baseline_statistics[2]
+        BASELINE = baseline_statistics[0]
+        BASELINE_STDE = baseline_statistics[2]
 
-        normalization_factor = baseline['UNC']
-
-        # total growth data
-        MRE_means = get_stats(data_set, 't').MRE
-        MRE_SD = get_stats(data_set, 't').MRE_SD
-        last_day_mean = MRE_means.iloc[-1]
-        last_day_SD = MRE_SD.iloc[-1]
-        growth = last_day_mean - baseline
-        growth_SD = (baseline_SD ** 2 + last_day_SD ** 2) ** 0.5
-
+        NORMALIZATION_FACTOR = BASELINE['UNC']
 
         # bar plot input
-        baseline_bar_heights = baseline / normalization_factor
-        baseline_bar_errors = baseline_SE / normalization_factor
+        baseline_bar_heights = BASELINE / NORMALIZATION_FACTOR
+        baseline_bar_errors = BASELINE_STDE / NORMALIZATION_FACTOR
 
-        growth_bar_heights = growth / baseline
+        # growth_bar_heights = growth / baseline
 
         labels = SOILS
 
         # plot baseline
-        baseline_bars = {}
         baseline_bars = plot_bars(baseline_axes, x_location,
                                   baseline_bar_heights, SOIL_WIDTH, labels, baseline_bar_errors)
 
-        # plot growth
-        growth_bars = plot_bars(growth_axes, x_location, growth_bar_heights, SOIL_WIDTH, labels, )
+        bars[category_name] = baseline_bars
+        # bars[category_name + '_growth'] = growth_bars
 
-        all_bars[category_name + '_baseline'] = baseline_bars
-        all_bars[category_name + '_growth'] = growth_bars
+    legend_handles = bars['MBC'].values()
+    legend_labels = bars['MBC'].keys()
+    basline_figure.legend(legend_handles, legend_labels, loc='center right')
 
-    legend_handles = all_bars['MBC_growth'].values()
-    legend_labels = all_bars['MBC_growth'].keys()
-    basline_growth_figure.legend(legend_handles, legend_labels, loc='center right')                                                                                   # baseline_category_names = [ key for key in all_bars.keys() if 'baseline' in key]
-                                                                                        ## remove rectangle patch of UNC
-    return basline_growth_figure                                                       # for category in baseline_category_names:
-                                                                                        #     for soil in all_bars[category].keys():
-                                                                                        #         if soil == 'UNC':
-                                                                                        #             bar = all_bars[category][soil]
-                                                                                        #             bar_rectangle = bar.patchs
-                                                                                        #             bar_rectangle.clear()
+    ## remove rectangle patch of UNC
+    # baseline_category_names = [ key for key in bars.keys() if 'baseline' in key]
+    # for category in baseline_category_names:
+    #     for soil in bars[category].keys():
+    #         if soil == 'UNC':
+    #             bar = bars[category][soil]
+    #             bar_rectangle = bar.patchs
+    #             bar_rectangle.clear()
 
+    return basline_figure
 
 def make_bar_axes(figure: Figure, x_locations, soil_width, y_label,
-                                                x_label=None, categories=None, axes_lineup=0) -> Axes:
+                                                x_label=None, categories=None) -> Axes:
 
-    position = (
-                111 if axes_lineup == 0 else
-                211 if axes_lineup == 1 else
-                212
-               )
+    # position = (
+    #             111 if axes_lineup == 0 else
+    #             211 if axes_lineup == 1 else
+    #             212
+    #            )
 
-    axes = figure.add_subplot(position)
+    axes: Axes = figure.add_subplot(111)
 
-    axes.set_ylabel(y_label, rotation=45, position=(20, 0.65), ha='right')
+    axes.set_ylabel(y_label, rotation=45, position=(20, 0.65), ha='right', ma='center')
+    axes.set_xlabel(x_label)
     axes.set_yticks([])
-
-    if categories is not None:
-        axes.set_xticks(x_locations + soil_width)
-        axes.set_xticklabels(categories)
-        axes.xaxis.set_tick_params(length=1, pad=20)
-        axes.set_xlabel(x_label)
-    else:
-        axes.set_xticks([])
+    axes.set_xticks(x_locations + soil_width)
+    axes.set_xticklabels(categories)
+    axes.xaxis.set_tick_params(length=1, pad=20)
 
     return axes
 
@@ -355,11 +335,7 @@ def plot_bars(axes: Axes, x_location, heights, width, labels, bar_error=None):
                            width=width,
                            yerr=bar_error[soil_label],
                            label=soil_label,
-                           color=(
-                                  COLORS[0] if (soil_label == 'ORG') else
-                                  COLORS[1] if (soil_label == 'MIN') else
-                                  COLORS[2]
-                                 )
+                           color=COLORS[soil_label]
                            )
         else:
             bar = axes.bar(
@@ -367,11 +343,7 @@ def plot_bars(axes: Axes, x_location, heights, width, labels, bar_error=None):
                            height=height,
                            width=width,
                            label=soil_label,
-                           color=(
-                                  COLORS[0] if (soil_label == 'ORG') else
-                                  COLORS[1] if (soil_label == 'MIN') else
-                                  COLORS[2]
-                                 ),
+                           color=COLORS[soil_label]
                            )
 
         i += width
@@ -409,8 +381,8 @@ def plot_c_to_n(data):
 
     axes = carbon_figure.add_subplot(111)
 
-    axes.xaxis.set_minor_locator(minor_locator)
-    axes.xaxis.set_major_locator(major_locator)
+    axes.xaxis.set_minor_locator(MINOR_LOCATOR)
+    axes.xaxis.set_major_locator(MAJOR_LOCATOR)
     axes.tick_params(axis='x', which='minor', width=1, length=3)
 
     lines = data.plot(ax=axes)
--- visualize.py
+++ visualize.py
@@ -2,7 +2,7 @@ from matplotlib import pyplot             # todo solve: running visualize.py wit
 
 from raw_data import get_setup_arguments, get_raw_data, get_multi_sets
 from stats import get_normalized, get_stats, get_carbon_stats
-from plot import make_figure, plot_dynamics, plot_all_parameters, plot_c_to_n
+from plot import make_figure, plot_dynamics, plot_baseline, plot_c_to_n
 # from model_dynamics import plot_model
 from helpers import get_week_ends
 # from Ttest import get_daily_Ttest
@@ -19,10 +19,9 @@ OUTPUT_DIRECTORY = '/home/elan/Dropbox/research/figures'
 
 # setup
 setup_arguments = get_setup_arguments()
+
 DATA_SETS_NAMES = setup_arguments.sets
 NUMBERS = setup_arguments.numbers
-
-# SOILS = ['ORG', 'MIN', 'UNC']
 
 # plot dynamics of each soil parameter as a seperate graph
 for set_name, number in zip(DATA_SETS_NAMES, NUMBERS):
@@ -30,14 +29,11 @@ for set_name, number in zip(DATA_SETS_NAMES, NUMBERS):
     # input data into DataFrame
     raw_data = get_raw_data(set_name)
 
-    # week_ends = get_week_ends(raw_data)
-
-    # get statistics
+    # get basic statistics
     stats = get_stats(raw_data, 't')
     normalized_stats = get_normalized(raw_data)
 
-    # data to plot
-
+    # statistics to plot
     means = stats.means
     stde = stats.stde
     stdv = stats.stdv
@@ -45,17 +41,6 @@ for set_name, number in zip(DATA_SETS_NAMES, NUMBERS):
     norm_means = normalized_stats.means
     norm_stde = normalized_stats.stde
     norm_stdv = normalized_stats.stdv
-
-    # means = BasicStats.means
-    # means_SE = BasicStats.means_SE
-    # MRE = BasicStats.MRE
-    # MRE_SD = BasicStats.MRE_SD
-    # MRE_SE = BasicStats.MRE_SE
-    # control = BasicStats.control
-    # control_SE = BasicStats.control_SE
-    # normalized = BasicStats.normalized_diff
-
-    # microbial_c_to_n = get_carbon_stats()
 
     # plot dynamics
     dynamics_figure = make_figure(raw_data, number, set_name)
@@ -66,33 +51,15 @@ for set_name, number in zip(DATA_SETS_NAMES, NUMBERS):
     dynamics_figure.savefig("%s/%s_normalized.png" % (OUTPUT_DIRECTORY, set_name))
     pyplot.cla()
 
-    # if set_name == 'RESP':
-    #     model_figure = plot_model(difference)
-    #     model_figure.savefig('./%s/RESP_model_3_weeks.png' % OUTPUT_DIRECTORY)
-
-    # # plot weekedns dynamics
-    # treatment_figure = plot_dynamics(MRE.loc[week_ends, :], MRE_SE.loc[week_ends, :],
-    #                                  MRE_SD.loc[week_ends, :], number, set_name,
-    #                                  normalized=normalized.loc[week_ends, :])
-    #
-    # treatment_figure.savefig("./%s/%s_model.png" % (OUTPUT_DIRECTORY, set_name))
-    # pyplot.cla()
-
-# # plot baseline
-# raw_data_sets = get_multi_sets(SETS_NAMES)
-# soil_properties_figure = plot_all_parameters(raw_data_sets)
-# soil_properties_figure.savefig('./%s/baseline.png' % OUTPUT_DIRECTORY)
+# plot baseline
+raw_data_sets = get_multi_sets(DATA_SETS_NAMES)
+soil_properties_figure = plot_baseline(raw_data_sets)
+soil_properties_figure.savefig('%s/baseline.png' % OUTPUT_DIRECTORY)
 
 # # plot C to N ratio
 # c_to_n = get_carbon_stats()
 # carbon_figure = plot_c_to_n(c_to_n)
 # carbon_figure.savefig('./%s/C_to_N.png' %OUTPUT_DIRECTORY)
-
-
-
-
-
-
 
 # MRE = MRE.loc[get_week_ends(MRE)] if set_name == 'RESP' else MRE
 # MRE_SE = MRE_SE.loc[get_week_ends(MRE_SE)] if set_name == 'RESP' else MRE_SE
