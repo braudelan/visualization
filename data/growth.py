a4ed07809470196b6987e6c97d6dfe170cc398cc
elan <braudelan@gmail.com>
2020-02-16 14:32:40 +0200

    adjusted growth function to be more generally applicable.

--- data/growth.py
+++ data/growth.py
@@ -8,13 +8,23 @@ Stats = namedtuple('Stats', ['means', 'stde'])
 SOILS = Constants.groups
 
 
-def get_raw_weekly_growth(raw_data):
+def get_raw_interval_growth(raw_data):
+
+    sampling_days = raw_data.index
+
+    first_day = sampling_days[0]
+    second_day = sampling_days[1]
+    second_to_last_day = sampling_days[-2]
+    last_day = sampling_days[-1]
+
+    interval_limits = [day for day in sampling_days if day in [0, 7, 14, 21, 28]]
+    number_of_intervals = len(interval_limits) - 1
 
     def rename_columns(dataframe):
 
         def get_renaming_map(dataframe):
             old = dataframe.columns
-            new = list(range(1, 5))
+            new = list(range(1, number_of_intervals + 1))
             zipped = zip(old, new)
             return dict(zipped)
 
@@ -22,44 +32,49 @@ def get_raw_weekly_growth(raw_data):
         dataframe.rename(mapper=rename_by,
                          axis='columns', inplace=True)
 
-    intervals_limits = get_week_ends(raw_data)
-    data = raw_data.loc[intervals_limits]
+    data = raw_data.loc[interval_limits]
     data = data.T
 
-    t_start = data.loc[:, 0:21]
-    t_end =  data.loc[:, 7:28]
+    interval_beginings = data.loc[:, first_day:second_to_last_day]
+    interval_endings =  data.loc[:, second_day:last_day]
 
     # rename column labels from interval limits to intervals (days to weeks)
     endings_beginings = [
-        t_end,
-        t_start
+        interval_endings,
+        interval_beginings
     ]
     for dataframe in endings_beginings:
         rename_columns(dataframe)
 
-    # get weekly growth
-    weekly_growth = t_end - t_start
+    # get growth
+    interval_growth = interval_endings - interval_beginings
 
     # change irregular values to none
-    weekly_growth.loc[('ORG', 2), 4] = None
-    weekly_growth = weekly_growth.droplevel(
+    # weekly_growth.loc[('ORG', 2), 4] = None
+    interval_growth = interval_growth.droplevel(
                                     'replicate')
-    weekly_growth = weekly_growth.rename_axis(
-                                    columns='week')
+    interval_growth = interval_growth.rename_axis(
+                                    columns='interval')
 
 
 
-    return weekly_growth
+    return interval_growth
 
 
-def get_weekly_growth(raw_data):
-    '''retrun means of weekly growth for every soil.'''
+def get_weekly_growth(raw_data, treatment):
+    '''
+    retrun means of weekly growth for every soil.
 
-    weekly_growth = get_raw_weekly_growth(raw_data)
+    :parameter treatment: str
+     't' for treated samples, 'c' for control samples.
+
+    '''
+
+    weekly_growth = get_raw_interval_growth(raw_data)
 
 
     stacked = weekly_growth.stack()
-    grouped_soil_week = stacked.groupby(['soil', 'week'])
+    grouped_soil_week = stacked.groupby(['soil', 'interval'])
     means = grouped_soil_week.mean()
     means = means.unstack().T
     stde = grouped_soil_week.sem()
@@ -86,7 +101,7 @@ if __name__ == '__main__':
 
     raw_data = get_raw_data('MBC')
     raw_data = baseline_normalize(raw_data)
-    weekly_growth = get_raw_weekly_growth(raw_data)
+    weekly_growth = get_raw_interval_growth(raw_data)
     for soil in SOILS:
         soil_weekly_growth = weekly_growth.loc[soil]
         booleans = significance_between_weeks(soil_weekly_growth)
